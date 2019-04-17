c5e3d71ddc476cc0978927527c4af489622ede14
Jacob Wilkins <jacobwilkins501@gmail.com>
2019-04-17 02:55:12 -0500

    Create Move.java
    Adding Move class

---
+++ Move.java
@@ -0,0 +1,20 @@
+public class Move {
+	int move;
+	int score;
+	
+	public void setScore(int scoreSet) {
+		score = scoreSet;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void setMove(int moveSet) {
+		move = moveSet;
+	}
+	
+	public int getMove() {
+		return move;
+	}
+}
