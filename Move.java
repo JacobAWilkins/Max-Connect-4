d165eb423419fe02e07c3be6a1d52ffbe7a13280
Jacob Wilkins <jacobwilkins501@gmail.com>
2019-06-04 22:08:11 -0500

    Update Move.java
    added header comment

--- Move.java
+++ Move.java
@@ -1,3 +1,25 @@
+/*H****************************************************************
+* FILENAME :        Move.java
+*
+* DESCRIPTION :
+*       Stores information about a particular move 
+*
+* PUBLIC FUNCTIONS :
+*       void     setScore( int )
+*       int      getScore( )
+*       void     setMove( int )
+*       int      getMove( )
+*
+* NOTES :
+*       Each move has a number representing the
+*       column and a score value.
+*
+*       Copyright 2019, Jacob Wilkins.  All rights reserved.
+* 
+* AUTHOR :    Jacob Wilkins        START DATE :    4 Mar 19
+*
+*H*/
+
 public class Move {
 	int move;
 	int score;
